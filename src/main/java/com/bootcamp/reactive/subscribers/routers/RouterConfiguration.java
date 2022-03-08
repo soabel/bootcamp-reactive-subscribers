3ec75c61f2dbba5f078435ba3dadff0e2f67b15a
soabel <alfredo.benaute@gmail.com>
2022-03-08 18:37:36 -0500

    add swagger

--- src/main/java/com/bootcamp/reactive/subscribers/routers/RouterConfiguration.java
+++ src/main/java/com/bootcamp/reactive/subscribers/routers/RouterConfiguration.java
@@ -1,6 +1,15 @@
 package com.bootcamp.reactive.subscribers.routers;
 
+import com.bootcamp.reactive.subscribers.entities.Subscriber;
 import com.bootcamp.reactive.subscribers.handlers.SubscriberHandler;
+import com.bootcamp.reactive.subscribers.services.SubscriberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -16,7 +25,28 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 @Configuration
 public class RouterConfiguration {
 
+
+
     @Bean
+        @RouterOperations(
+            {
+
+                @RouterOperation(path = "/subscribers",
+                        produces = {MediaType.APPLICATION_JSON_VALUE},
+                        method = RequestMethod.GET,
+                        beanClass = SubscriberService.class,
+                        beanMethod = "findAll",
+                        operation = @Operation(
+                                operationId = "findAll",
+                                responses = {
+                                        @ApiResponse(responseCode = "200",
+                                                description = "successful operation",
+                                                content = @Content(array=@ArraySchema(schema = @Schema(implementation = Subscriber.class))))},
+                                parameters = {})
+
+                )
+
+            })
     public RouterFunction<ServerResponse> blogRoutes(SubscriberHandler subscriberHandler) {
         return RouterFunctions.nest(RequestPredicates.path("/subscribers"),
                 RouterFunctions
